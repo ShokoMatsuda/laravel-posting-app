78f2f55b3ef2e05a7fb93f90b44e351c01b91b42
ShokoMatsuda <syokom.0128@gmail.com>
2024-02-11 19:43:28 +0900

    8.5 投稿編集ページへのリンクを追加しよう

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -29,7 +29,7 @@
         @if (session('error_message'))
             <p>{{ session('error_message') }}</p>
         @endif
-        
+
         <a href="{{ route('posts.create') }}">新規投稿</a>
 
         @if($posts->isNotEmpty())
@@ -38,6 +38,7 @@
                     <h2>{{ $post->title }}</h2>
                     <p>{{ $post->content }}</p>
                     <a href="{{ route('posts.show', $post) }}">詳細</a>
+                    <a href="{{ route('posts.edit', $post) }}">編集</a>
                 </article>
                 @endforeach
             @else
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -25,12 +25,16 @@
         @if (session('flash_message'))
             <p>{{ session('flash_message') }}</p>
         @endif
-        
+
         <a href="{{ route('posts.index') }}">&lt; 戻る</a>
 
         <article>
             <h2>{{ $post->title }}</h2>
             <p>{{ $post->content }}</p>
+
+            @if ($post->user_id === Auth::id())
+                <a href="{{ route('posts.edit', $post) }}">編集</a>
+            @endif
         </article>
     </main>
     <footer>
